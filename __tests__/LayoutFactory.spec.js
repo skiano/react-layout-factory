e12cff323255e64b385ba54d70d954af62c29cc1
Greg Skiano <gregoryskiano@gmail.com>
2014-12-19 19:49:07 -0500

    setting up files

---
+++ __tests__/LayoutFactory.spec.js
@@ -0,0 +1,17 @@
+
+/** @jsx React.DOM */
+
+// jest.dontMock('../lib/LayoutFactory');
+
+var React = require('react/addons')
+  , TestUtils = React.addons.TestUtils
+  // , LayoutFactory = require('../lib/LayoutFactory')
+  ;
+
+describe('Layout Factory', function () {
+
+  it('should work', function () {
+
+  });
+
+});
