114f6d4b99be37ee04f170b5f733ac1beda361c0
Greg Skiano <gregoryskiano@gmail.com>
2014-12-19 20:06:56 -0500

    decided to go with creating a wrapper component

--- __tests__/LayoutFactory.spec.js
+++ __tests__/LayoutFactory.spec.js
@@ -1,16 +1,34 @@
 
 /** @jsx React.DOM */
 
-// jest.dontMock('../lib/LayoutFactory');
+jest.dontMock('../lib/LayoutFactory');
 
 var React = require('react/addons')
   , TestUtils = React.addons.TestUtils
-  // , LayoutFactory = require('../lib/LayoutFactory')
+  , LayoutFactory = require('../lib/LayoutFactory')
   ;
+
+var Component, Wrapped;
 
 describe('Layout Factory', function () {
 
+  beforeEach(function () {
+
+    Component = React.createClass({
+      render: function () {
+        return (<div>Component</div>);
+      }
+    });
+
+  });
+
   it('should work', function () {
+
+    Wrapped = LayoutFactory(Component, {});
+    
+    var test = React.renderToString((<Wrapped/>));
+
+    console.log(test);
 
   });
 
